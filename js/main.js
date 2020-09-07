cbac813e3303e89ed122da66b64a4d0dbd292c68
Eudy Reynoso <servero2816@gmail.com>
2020-09-07 12:45:57 -0400

    CSS finalizado

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 //Menu class selector
 const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu');
+const btn_descargar = document.querySelector('.btn-get');
+const btn_Phone = document.querySelector('.btn-phone');
 
 menuIcon.addEventListener('click', () =>{
 
     menuIcon.classList.toggle('active');
     menu.classList.toggle('active');
 });
+
+btn_Phone.addEventListener('click', () =>{
+    swal.fire({
+        title:"whatsapp",
+        html:'<i class="AlertIcon fab fa-whatsapp"></i>',
+        footer: 'Disponible todo el dia!'
+    })
+})
