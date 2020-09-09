63f0c3a30ed662b000f871bdd3221bc85b5a82b3
Eudy Reynoso <servero2816@gmail.com>
2020-09-09 14:59:04 -0400

    CSS, se cambiao las fuentes se añadio nuevo fondo y la libreria de alertas

--- js/main.js
+++ js/main.js
@@ -2,18 +2,26 @@
 const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu');
 const btn_descargar = document.querySelector('.btn-get');
+const overlay = document.querySelector('.overlay');
 const btn_Phone = document.querySelector('.btn-phone');
 
 menuIcon.addEventListener('click', () =>{
 
     menuIcon.classList.toggle('active');
     menu.classList.toggle('active');
+    overlay.classList.toggle('active');
 });
 
 btn_Phone.addEventListener('click', () =>{
     swal.fire({
-        title:"whatsapp",
-        html:'<i class="AlertIcon fab fa-whatsapp"></i>',
-        footer: 'Disponible todo el dia!'
+        title:'<i class="AlertIcon fab fa-whatsapp"></i>',
+        html:'<span class="end-alert">829-682-9541</span>',
+         // html:<span class="end-alert">¡Disponible todo el dia!</span>,
+        // html:'<i class="AlertIcon fab fa-whatsapp"></i>',
+        footer: '<span class="end-alert">¡Disponible todo el dia!</span>',
+        //width: "0%",
+        position: "top",
+        confirmButtonText:"Ok",
+        confirmButtonColor: "#ff4b5c"
     })
 })
